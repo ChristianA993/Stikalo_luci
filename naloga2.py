565f402b6f831619ee3363b4f4fb52f6422d05e1
Zephyrus <adam.christjan@gmail.com>
2024-01-18 22:00:40 +0100

    Urejeno komentiranje

--- naloga2.py
+++ naloga2.py
@@ -1,15 +1,21 @@
+"""
+Aplikacija za vklop/izklop luči s stikalom. Računa koliko časa je luč prižgana oz. stikalo v ON poziciji
+
+"""
+
 from flask import Flask, render_template, jsonify, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index2.html')  # Prikaz glavne HTML strani
 
 @app.route('/toggle/<state>')
 def toggle(state):
     if state == 'on':
         return jsonify({'image': url_for('static', filename='light_on.jpg'), 'state': 'off'})
     return jsonify({'image': url_for('static', filename='light_off.jpg'), 'state': 'on'})
+    # Vrnitev podatkov v JSON formatu glede na trenutno stanje svetlobe
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Zagon Flask aplikacije v načinu razhroščevanja
