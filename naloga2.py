2f278b5df313788b7d85fff5865680f463734ea9
Zephyrus <adam.christjan@gmail.com>
2024-01-22 21:26:53 +0100

    Dodani komentarji + css

--- naloga2.py
+++ naloga2.py
@@ -3,19 +3,23 @@
 from flask import Flask, render_template, jsonify, url_for
 
 
-# GLOBAL VARIABLES
+# GLOBALNE SPREMENLJIVKE
 # ------------------------------------------------
-consumption_data = []   # Globalni seznam
-total_Wh = 0            # Global variable to store total watt-hours
+consumption_data = []   # Globalni seznam, ki shranjuje podatke o porabi
+total_Wh = 0            # Globalna spremenljivka za shranjevanje skupnih vat-ur
 
 
-# FUNCTIONS
+# FUNKCIJE
 # ------------------------------------------------
 def print_consumption_data():
+    # Ta funkcija izpiše trenutne podatke o porabi.
+    # Izpisuje podatke za vsak vnos posebej in izračuna skupne vat-ure.
     global consumption_data, total_Wh
     print("\nConsumption Data List:\n" + '-' * 50)
+
     for index, data in enumerate(consumption_data):
         print(f"Entry {index + 1}: Time - {data['time']}s, Watts - {data['watts']}, Watt-hours - {data['watt-hours']:.3f}")
+
     print('-' * 50 + f"\nTotal Watt-Hours: {total_Wh:.3f}\n")
 
 
@@ -26,11 +30,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')  # Prikaz glavne HTML strani
+    # Ta pot prikaže glavno HTML stran.
+    return render_template('index2.html')
 
 
 @app.route('/toggle/<state>/<time>/<watts>')
 def export_to_list(state, time, watts):
+    # Ta pot upravlja vklop/izklop in spremlja porabo.
+    # Ko se luč izklopi, shranjuje čas, vate in izračuna vat-ure.
     global total_Wh
     if state == 'on':
         image_file = 'light_on.jpg'
@@ -42,8 +49,8 @@ def export_to_list(state, time, watts):
                 "watts": int(watts),
                 "watt-hours": watt_hours
             })
-            total_Wh += watt_hours  # Add to the total watt-hours
-            print_consumption_data()
+            total_Wh += watt_hours      # Dodajanje k skupnim vat-uram
+            print_consumption_data()    # Izpis seznama porabe
 
         image_file = 'light_off.jpg'
     return jsonify({'image': url_for('static', filename=image_file), 'state': 'off' if state == 'on' else 'on'})
@@ -51,13 +58,12 @@ def export_to_list(state, time, watts):
 
 @app.route('/get_consumption_data')
 def get_consumption_data():
+    # Ta pot vrača trenutne podatke o porabi za prikaz na front-end.
     global total_Wh
     return jsonify({'data': consumption_data, 'total_Wh': total_Wh})
 
 
-# MAIN
+# GLAVNI DEL
 # ------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)  # Zagon Flask aplikacije v načinu razhroščevanja
-
-
+    app.run(debug=True)  # Zagon Flask aplikacije v načinu za razhroščevanje
