974c13a34650249d87d6d801ea8595a492fe719f
Zephyrus <adam.christjan@gmail.com>
2024-01-31 17:10:50 +0100

    Changes 2

--- naloga2.py
+++ naloga2.py
@@ -1,6 +1,7 @@
 # IMPORT
 # ------------------------------------------------
 from flask import Flask, render_template, jsonify, url_for
+from datetime import datetime  # Add this import at the top
 
 
 # GLOBALNE SPREMENLJIVKE
@@ -34,23 +35,43 @@ def index():
     return render_template('index2.html')
 
 
+# @app.route('/toggle/<state>/<time>/<watts>')
+# def export_to_list(state, time, watts):
+#     # Ta pot upravlja vklop/izklop in spremlja porabo.
+#     # Ko se luč izklopi, shranjuje čas, vate in izračuna vat-ure.
+#     global total_Wh
+#     if state == 'on':
+#         image_file = 'light_on.png'
+#     else:
+#         if time != "0":
+#             watt_hours = (int(time) / 3600) * int(watts)
+#             consumption_data.append({
+#                 "time": int(time),
+#                 "watts": int(watts),
+#                 "watt-hours": watt_hours
+#             })
+#             total_Wh += watt_hours      # Dodajanje k skupnim vat-uram
+#             print_consumption_data()    # Izpis seznama porabe
+#
+#         image_file = 'light_off.png'
+#     return jsonify({'image': url_for('static', filename=image_file), 'state': 'off' if state == 'on' else 'on'})
 @app.route('/toggle/<state>/<time>/<watts>')
 def export_to_list(state, time, watts):
-    # Ta pot upravlja vklop/izklop in spremlja porabo.
-    # Ko se luč izklopi, shranjuje čas, vate in izračuna vat-ure.
     global total_Wh
     if state == 'on':
         image_file = 'light_on.png'
     else:
         if time != "0":
+            current_time = datetime.now()  # Capture the current date and time
             watt_hours = (int(time) / 3600) * int(watts)
             consumption_data.append({
                 "time": int(time),
                 "watts": int(watts),
-                "watt-hours": watt_hours
+                "watt-hours": watt_hours,
+                "timestamp": current_time.strftime("%Y-%m-%d %H:%M:%S")  # Store formatted timestamp
             })
-            total_Wh += watt_hours      # Dodajanje k skupnim vat-uram
-            print_consumption_data()    # Izpis seznama porabe
+            total_Wh += watt_hours
+            print_consumption_data()
 
         image_file = 'light_off.png'
     return jsonify({'image': url_for('static', filename=image_file), 'state': 'off' if state == 'on' else 'on'})
@@ -61,6 +82,18 @@ def get_consumption_data():
     # Ta pot vrača trenutne podatke o porabi za prikaz na front-end.
     global total_Wh
     return jsonify({'data': consumption_data, 'total_Wh': total_Wh})
+
+
+@app.route('/delete_entry/<int:index>', methods=['POST'])
+def delete_entry(index):
+    global consumption_data, total_Wh
+    try:
+        # Remove the specified entry from the list
+        removed_entry = consumption_data.pop(index)
+        total_Wh -= removed_entry['watt-hours']  # Adjust the total watt-hours
+        return jsonify({'success': True, 'total_Wh': total_Wh})
+    except IndexError:
+        return jsonify({'success': False, 'error': 'Index out of range'}), 400
 
 
 # GLAVNI DEL
